6afae86386768f84217b3a57b7b06a00f1c24d6c
taconger <taconger@students.unwsp.edu>
2024-11-01 14:28:19 -0500

    Handles ValueError and IOError

--- program_3.py
+++ program_3.py
@@ -7,17 +7,27 @@ def main():
     sumNumberInTxtFile = sum_numbers_from_file()
     print("The sum of all the numbers in 'numbers.txt' is : ", sumNumberInTxtFile)
 
-
+#FUNCTION to sum number from a file given the file name
+#TRY CATCH open file as read, if unable except IOError
+#TRY CATCH total sum, if unable except ValueError
+#RETURN TOTAL sum
+#OR RETURN 
 def sum_numbers_from_file():
     total = 0
-    with open('numbers.txt', 'r') as file3:
-        for line in file3:
-            for number in line.split():
+    try:
+        with open('numbers.txt', 'r') as file:
+            for line in file:
                 try:
-                    total += float(number)
+                    number = float(line)
+                    total += number
                 except ValueError:
-                    pass 
-    return total
+                    print(f"Warning: Skipping invalid number in file")
+                    continue
+        return total
+            
+    except IOError as e:
+        print(f"Error reading file: {e}")
+        return 0
 
 if __name__ == '__main__':
     main()
